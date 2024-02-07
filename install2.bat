dc70d87d7da9035a80d2aacb3d6b0ceab0a9b43a
patukista <155352616+d-dragon-project@users.noreply.github.com>
2024-02-07 17:25:20 +0800

    Update install2.bat

--- install2.bat
+++ install2.bat
@@ -1,5 +1,4 @@
 @echo off
-
 mkdir c:\dsdt
 c: & cd \dsdt
 set 64bit_OS_asl="C:\Program Files (x86)\Windows Kits\10\Tools\x64\ACPIVerify\asl.exe"
@@ -10,7 +9,9 @@ asl /u dsdt.dat
 copy dsdt.asl dsdt-modified.asl
 iasl -da -dl -fe refs.txt dsdt.dat
 copy dsdt.dsl dsdt-modified.dsl
+c: & cd \winaudiomac
 copy /y dsdt-modified.dsl c:\dsdt
+c: & cd \dsdt
 iasl -ve dsdt-modified.dsl
 asl /loadtable dsdt-modified.aml  
 bcdedit -set TESTSIGNING ON
