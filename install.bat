6c6aedaf1a7f15f5110234be776e3d863a25af81
patukista <155352616+d-dragon-project@users.noreply.github.com>
2024-02-07 10:59:02 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,33 +1,32 @@
-@echo on
-
+@echo off
+echo Create c:\dsdt folder
 mkdir c:\dsdt
+echo off
 c: & cd \dsdt
+echo Extract Windows Binary Tools (WBT - Dec 2016) to c:\dsdt directory. 
 xcopy c:\winaudiomac\WBT\*.* c:\dsdt
 c: & cd \windows\system32
 set 64bit_OS_asl="C:\Program Files (x86)\Windows Kits\10\Tools\x64\ACPIVerify\asl.exe"
 set 32bit_OS_asl="C:\Program Files (x86)\Windows Kits\10\Tools\x86\ACPIVerify\asl.exe"
 copy /y %32bit_OS_asl% c:\dsdt > nul & copy /y %64bit_OS_asl% c:\dsdt > nul
 if not exist c:\dsdt\asl.exe echo ERROR: Failed to copy asl.exe to c:\dsdt
-#cd c:\winaudiomac
-#copy /y asl.exe c:\dsdt
 cd c:\dsdt
 @echo off
 pause
 
-
+echo Dump your ACPI tables to disk files (dsdt.asl and dsdt.dat) 
 cd c:\dsdt
 @echo on
 acpidump -b -z
 asl /u dsdt.dat
 copy dsdt.asl dsdt-modified.asl
-
 @echo off
 dir dsdt-modified.asl
 if not exist c:\dsdt\dsdt-modified.asl echo ERROR: Failed to copy refs.txt to c:\dsdt echo FILE EXIST
 
+echo Use the Intel method and Copy refs.txt to c:\dsdt
 cd c:\winaudiomac
 @echo on
-echo Copy refs.txt to c:\dsdt
 copy /y refs.txt c:\dsdt
 @echo off
 dir refs.txt 
@@ -45,23 +44,21 @@ pause
 @echo on
 echo "Modify dsdt-modified.dsl
 @echo off
-cd c:\winaudiomac
+c: & cd \winaudiomac
 copy /y dsdt-modified.dsl c:\dsdt
 
 
 @echo off
-cd c:\dsdt
+c: & cd \dsdt
+echo Create a dsdt-modified.aml file . The  '-ve' disables warning messages.
 @echo on
 iasl -ve dsdt-modified.dsl
-pause
-
-
 @echo off
 c: & cd \dsdt
 dir dsdt-modified.aml
 if not exist c:\dsdt\dsdt-modified.aml echo ERROR: Failed to copy dsdt-modified.dsl to c:\dsdt echo FILE EXIST
 @echo on
-asl /loadtable dsdt-modified.aml
+asl /loadtable dsdt-modified.aml  
 
 @echo off
 echo Enable TESTSIGNING mode for the registry override to apply. At the Command Prompt (admin) type:
