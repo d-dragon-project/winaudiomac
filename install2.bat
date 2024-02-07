6347411c6d9661668f4f19ac606981927ddd61fe
patukista <155352616+d-dragon-project@users.noreply.github.com>
2024-02-07 00:48:53 -0800

    Add files via upload

---
+++ install2.bat
@@ -0,0 +1,41 @@
+@echo off
+
+mkdir c:\dsdt
+c: & cd \dsdt
+set 64bit_OS_asl="C:\Program Files (x86)\Windows Kits\10\Tools\x64\ACPIVerify\asl.exe"
+set 32bit_OS_asl="C:\Program Files (x86)\Windows Kits\10\Tools\x86\ACPIVerify\asl.exe"
+copy /y %32bit_OS_asl% c:\dsdt > nul & copy /y %64bit_OS_asl% c:\dsdt > nul
+
+cd c:\dsdt 
+xcopy c:\winaudiomac\WBT\*.* c:\dsdt
+copy c:\winaudiomac\asl.exe c:\dsdt
+acpidump -b -z
+asl /u dsdt.dat
+copy dsdt.asl dsdt-modified.asl
+
+cd c:\winaudiomac
+copy /y refs.txt c:\dsdt
+
+
+c: & cd \dsdt
+iasl -da -dl -fe refs.txt dsdt.dat
+copy dsdt.dsl dsdt-modified.dsl
+
+
+c: & cd \winaudiomac
+copy /y dsdt-modified.dsl c:\dsdt
+
+
+c: & cd \dsdt
+iasl -ve dsdt-modified.dsl
+
+c: & cd \dsdt
+dir dsdt-modified.aml
+asl /loadtable dsdt-modified.aml  
+
+
+c: & cd \dsdt
+bcdedit -set TESTSIGNING ON
+c: & cd \windows\system32
+bcdedit -set TESTSIGNING ON
+exit
